30a2810a0e5473ef23eea825a3a508726481f610
StephanieKimani <reubenmbalanya@gmail.com>
2023-07-17 22:20:50 +0300

    This is te start of 2D Rendering

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "On"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -25,53 +25,6 @@ project "GLFW"
 		"src/vulkan.c",
 		"src/window.c",
 	}
-
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:macosx"
-		pic "On"
-
-		files
-		{
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_COCOA"
-		}
 
 	filter "system:windows"
 		systemversion "latest"
@@ -103,9 +56,3 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
-
